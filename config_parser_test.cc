fa262865d46a794720dca698b42086e43194273c
Iris Liu <iliu@caltech.edu>
2014-04-17 14:37:12 -0700

    Added unit test

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,43 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+
+TEST(NginxConfigParserTest, ProxyConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("proxy_config", &out_config);
+
+  EXPECT_TRUE(success);
+}
+
+TEST(NginxConfigParserTest, NotNestedConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool fail = parser.Parse("hello world!", &out_config);
+
+  EXPECT_FALSE(fail);
+}
+
+TEST(NginxConfigParserTest, MoreComplexConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("nginxExample_config", &out_config);
+
+  EXPECT_TRUE(success);
+}
+
+// This is the reason the above complex config failed
+TEST(NginxConfigParserTest, NestedConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("foo { bar { sweet; }}", &out_config);
+
+  EXPECT_TRUE(success);
+}
+
+
